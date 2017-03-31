6d64eb11c82f99c18de41c7e9605ab9d4be7177b
Philip Kirkbride <kirkins@gmail.com>
2017-03-31 00:38:08 +0000

    random person working

--- lib/DDG/Goodie/RandomName.pm
+++ lib/DDG/Goodie/RandomName.pm
@@ -11,27 +11,43 @@ zci answer_type => "randomname";
 zci is_cached   => 0;
 
 handle query => sub {
+    my $query = $_;
     my $person = Data::RandomPerson->new()->create();
     my $name = "$person->{firstname} $person->{lastname}";
-    my %genders = (m => 'Male', f => 'Female');
-    my $string_answer = "Name: $name\nGender: $genders{$person->{gender}}\nDate of birth: $person->{dob}\nAge: $person->{age}";
+    if (index($query, 'person') != -1) {
+        my %genders = (m => 'Male', f => 'Female');
+        my $string_answer = "Name: $name\nGender: $genders{$person->{gender}}\nDate of birth: $person->{dob}\nAge: $person->{age}";
+        return $string_answer,
+            structured_answer => {
+                data => {
+                	name => $name,
+                 	gender => $genders{$person->{gender}},
+                 	date_of_birth => $person->{dob},
+                 	age => $person->{age}
+                },
+                templates => {
+                    group => 'text',
+                    options => {
+                    	content => 'DDH.random_name.content'
+                    }
+                }
+            };
 
-    return $string_answer,
-    	structured_answer => {
-    	data => {
-    		name => $name,
-		gender => $genders{$person->{gender}},
-		date_of_birth => $person->{dob},
-		age => $person->{age}
-    	},
-    	templates => {
-    		group => 'text',
-    		options => {
-    			content => 'DDH.random_name.content'
-    		}
-    	}
-    };
-
+    } else {
+        my $string_answer = "Name: $name";
+        return $string_answer,
+            structured_answer => {
+                data => {
+                	name => $name
+                },
+                templates => {
+                    group => 'text',
+                    options => {
+                    	content => 'DDH.random_name.content'
+                    }
+                }
+            };
+    }
 };
 
 
